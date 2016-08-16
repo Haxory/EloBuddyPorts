b4cb92633000201b96c948d01d635200cdf069ba
Haxory <monnqer@gmail.com>
2016-08-16 21:02:04 +0200

    Add NabbTracker
    ExpBar temp disabled, fixed positioning

---
+++ NabbTracker/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using EloBuddy;
+
+#pragma warning disable 1587
+
+namespace NabbTracker
+{
+    using LeagueSharp;
+    using LeagueSharp.SDK;
+
+    /// <summary>
+    ///     The application class.
+    /// </summary>
+    internal class Program
+    {
+        #region Methods
+
+        /// <summary>
+        ///     The entry point of the application.
+        /// </summary>
+        private static void Main()
+        {
+            /// <summary>
+            ///     Loads the Bootstrap.
+            /// </summary>
+            Bootstrap.Init();
+            Events.OnLoad += (sender, eventArgs) =>
+                {
+                    /// <summary>
+                    ///     Loads the assembly.
+                    /// </summary>
+                    Tracker.OnLoad();
+
+                    /// <summary>
+                    ///     Tells the player the assembly has been loaded.
+                    /// </summary>
+                    Chat.Print(
+                        "[SDK]<b><font color='#228B22'>Nabb</font></b>Tracker: <font color='#228B22'>Ultima</font> - Loaded!");
+                    Chat.Print(
+                        "<b><font color='#FF4000'>[WARNING]</font></b> Expbar is temporarily disabled.");
+                };
+        }
+
+        #endregion
+    }
+}
