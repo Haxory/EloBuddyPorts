e8b263539676ef83453d748aedb360b97d956219
Haxory <monnqer@gmail.com>
2016-08-17 11:52:15 +0200

    Add OKTB & HTracker

---
+++ Hikigaya/HTrackerSDK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.SDK;
+
+namespace HTrackerSDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Events.OnLoad += OnLoad;
+        }
+        private static void OnLoad(object sender, EventArgs e)
+        {
+            Tracker.OnLoad();
+            WardTracker.OnLoad();
+            PathTracker.OnLoad();
+        }
+    }
+}
